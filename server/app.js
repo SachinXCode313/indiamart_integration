dac1637bdf37c66e790130ea9b19f5f16680ee75
sg313 <sachin914597@gmail.com>
2024-08-08 16:51:03 +0530

    server side changes

--- server/app.js
+++ server/app.js
@@ -21,25 +21,25 @@ app.post('/indiamart/:secret_key', (req, res) => {
         console.log('Received data:', data);
         console.log(secretKey);
 
-        // Define the response object based on the received data
-        let response;
-        switch (data.CODE) {
-            case 200:
-                response = { code: 200, status: 'Success' };
-                break;
-            case 400:
-                response = { code: 400, status: 'Missing parameters' };
-                break;
-            case 500:
-                response = { code: 500, status: 'Error in connecting to the URL' };
-                break;
-            default:
-                response = { code: 500, status: 'Unknown error' };
-                break;
-        }
+        // // Define the response object based on the received data
+        // let response;
+        // switch (data.CODE) {
+        //     case 200:
+        //         response = { code: 200, status: 'Success' };
+        //         break;
+        //     case 400:
+        //         response = { code: 400, status: 'Missing parameters' };
+        //         break;
+        //     case 500:
+        //         response = { code: 500, status: 'Error in connecting to the URL' };
+        //         break;
+        //     default:
+        //         response = { code: 500, status: 'Unknown error' };
+        //         break;
+        // }
 
         // Set the appropriate HTTP status code and response headers
-        res.status(response.code).json(response);
+        res.status(200).json("Success");
     } catch (error) {
         console.error('Error processing request:', error);
         res.status(500).json({ code: 500, status: 'Error', message: 'Internal Server Error' });
