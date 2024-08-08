4cf5bcd0d41b6f901dc9b8f07f864cfaa16009fc
sg313 <sachin914597@gmail.com>
2024-08-08 14:03:29 +0530

    Intial Commit

--- server/app.js
+++ server/app.js
@@ -9,23 +9,23 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
-const corsConfig = {
-    origin: ['http://localhost:3000' , 'https://trip-plan-one.vercel.app'], // Allow requests from this origin
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
-    allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
-    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
-};
+// const corsConfig = {
+//     origin: ['http://localhost:3000' , 'https://trip-plan-one.vercel.app'], // Allow requests from this origin
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+//     allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+//     credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+// };
 
-connectDB();
+// connectDB();
 
 // Middleware to handle JSON request bodies
 app.use(express.json());
 
 // CORS configuration
-app.use(cors(corsConfig));
+// app.use(cors(corsConfig));
 
 // Handle preflight requests for all routes
-app.options('*', cors(corsConfig));
+// app.options('*', cors(corsConfig));
 
 // Test route to check if the server is running
 app.use('/test', (req, res) => {
