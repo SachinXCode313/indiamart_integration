fe84ef5389e9e5215df62ef1179459657829a0ae
sg313 <sachin914597@gmail.com>
2024-08-08 17:00:52 +0530

    backend setup

--- server/app.js
+++ server/app.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+dotenv.config()
+
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT ;
 
 // Middleware to parse JSON request bodies
 app.use(bodyParser.json());
@@ -21,25 +24,25 @@ app.post('/indiamart/:secret_key', (req, res) => {
         console.log('Received data:', data);
         console.log(secretKey);
 
-        // // Define the response object based on the received data
-        // let response;
-        // switch (data.CODE) {
-        //     case 200:
-        //         response = { code: 200, status: 'Success' };
-        //         break;
-        //     case 400:
-        //         response = { code: 400, status: 'Missing parameters' };
-        //         break;
-        //     case 500:
-        //         response = { code: 500, status: 'Error in connecting to the URL' };
-        //         break;
-        //     default:
-        //         response = { code: 500, status: 'Unknown error' };
-        //         break;
-        // }
+        // Define the response object based on the received data
+        let response;
+        switch (data.CODE) {
+            case 200:
+                response = { code: 200, status: 'Success' };
+                break;
+            case 400:
+                response = { code: 400, status: 'Missing parameters' };
+                break;
+            case 500:
+                response = { code: 500, status: 'Error in connecting to the URL' };
+                break;
+            default:
+                response = { code: 500, status: 'Unknown error' };
+                break;
+        }
 
         // Set the appropriate HTTP status code and response headers
-        res.status(200).json({ code: 200, status: 'Success' });
+        res.status(response.code).json(response);
     } catch (error) {
         console.error('Error processing request:', error);
         res.status(500).json({ code: 500, status: 'Error', message: 'Internal Server Error' });
