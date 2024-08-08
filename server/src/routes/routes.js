cf2e90d0e28573fa7b4823ba24ea28a7e5510b05
sg313 <sachin914597@gmail.com>
2024-08-08 14:12:11 +0530

    Intial Commit

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -1,21 +1,21 @@
-import express from "express";
-// import {createTripPlan}  from "../controllers/UserControllerMongoDB.js";
-import {getTripPlan,createTripPlan,updateTripPlan,getEmployee,deletePlan}  from "../controllers/UserControllerSheet.js";
+// import express from "express";
+// // import {createTripPlan}  from "../controllers/UserControllerMongoDB.js";
+// import {getTripPlan,createTripPlan,updateTripPlan,getEmployee,deletePlan}  from "../controllers/UserControllerSheet.js";
 
 
-const routers = express.Router();
+// const routers = express.Router();
 
-routers.get('/get',getTripPlan)
+// routers.get('/get',getTripPlan)
 
-routers.post('/create',createTripPlan)
+// routers.post('/create',createTripPlan)
 
-routers.put('/update/:id',updateTripPlan)
+// routers.put('/update/:id',updateTripPlan)
 
-routers.get('/getEmployee',getEmployee)
+// routers.get('/getEmployee',getEmployee)
 
-routers.post('/deletePlan',deletePlan)
+// routers.post('/deletePlan',deletePlan)
 
-// routers.delete('/delete/:id',deleteUser)
+// // routers.delete('/delete/:id',deleteUser)
 
-export default routers;
+// export default routers;
 
