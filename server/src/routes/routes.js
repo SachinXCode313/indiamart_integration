5be83ffc24a911d6fcb4a9421093ad4aea622b97
sg313 <sachin914597@gmail.com>
2024-08-09 10:18:30 +0530

    backend setup

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -4,17 +4,17 @@ import express from "express";
 
 const routers = express.Router();
 
-routers.get('/indiamart/',(req,res) => {
-    // const { CODE, STATUS, RESPONSE } = req.body;
+routers.post('/indiamart/:key',(req,res) => {
+    console.log("api is called :) ");
+    const { CODE, STATUS, RESPONSE } = req.body;
     
-    // if (CODE === 200 && STATUS === 'SUCCESS') {
-    //     console.log('Lead received:', RESPONSE);
-    //     // Handle the lead data here
-    //     res.status(200).json({ code: 200, status: 'Success' });
-    // } else {
-    //     res.status(400).json({ code: 400, status: 'Failure' });
-    // }
-    res.send("indiamart working")
+    if (CODE === 200 && STATUS === 'SUCCESS') {
+        console.log('Lead received:', RESPONSE);
+        // Handle the lead data here
+        res.status(200).json({ code: 200, status: 'Success' });
+    } else {
+        res.status(400).json({ code: 400, status: 'Failure' });
+    }
 })
 
 export default routers;
