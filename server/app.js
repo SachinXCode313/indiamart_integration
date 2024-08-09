b7529cc1b35e9c673c87547b03c5d42ac545e2fa
sg313 <sachin914597@gmail.com>
2024-08-09 10:33:35 +0530

    backend setup

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express'
 import bodyParser from 'body-parser';
-import routers from './src/routes/routes.js';
+// import routers from './src/routes/routes.js';
 // import axios from 'axios';
 const app = express();
 dotenv.config();
@@ -14,7 +14,20 @@ app.get("/",(req,res) => {
     res.send("Server is working")
 })
 
-app.use('/api', routers);
+app.get('/api/indiamart/',(req,res) => {
+  console.log("api is called :) ");
+  // const { CODE, STATUS, RESPONSE } = req.body;
+  
+  // if (CODE === 200 && STATUS === 'SUCCESS') {
+  //     console.log('Lead received:', RESPONSE);
+  //     // Handle the lead data here
+  //     res.status(200).json({ code: 200, status: 'Success' });
+  // } else {
+  //     res.status(400).json({ code: 400, status: 'Failure' });
+  // }
+})
+
+// app.use('/api', routers);
 
 app.listen(port, () => {
   console.log(`Middleware listening at https://localhost:${port}`);
--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -1,21 +1,21 @@
-import express from "express";
-// import {createTripPlan}  from "../controllers/UserControllerMongoDB.js";
+// import express from "express";
+// // import {createTripPlan}  from "../controllers/UserControllerMongoDB.js";
 
 
-const routers = express.Router();
+// const routers = express.Router();
 
-routers.post('/indiamart/:key',(req,res) => {
-    console.log("api is called :) ");
-    const { CODE, STATUS, RESPONSE } = req.body;
+// routers.post('/indiamart/:key',(req,res) => {
+//     console.log("api is called :) ");
+//     const { CODE, STATUS, RESPONSE } = req.body;
     
-    if (CODE === 200 && STATUS === 'SUCCESS') {
-        console.log('Lead received:', RESPONSE);
-        // Handle the lead data here
-        res.status(200).json({ code: 200, status: 'Success' });
-    } else {
-        res.status(400).json({ code: 400, status: 'Failure' });
-    }
-})
+//     if (CODE === 200 && STATUS === 'SUCCESS') {
+//         console.log('Lead received:', RESPONSE);
+//         // Handle the lead data here
+//         res.status(200).json({ code: 200, status: 'Success' });
+//     } else {
+//         res.status(400).json({ code: 400, status: 'Failure' });
+//     }
+// })
 
-export default routers;
+// export default routers;
 
