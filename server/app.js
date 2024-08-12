2c12888b7e1a712c7bd8710b8ccb96288aee2a5a
sg313 <sachin914597@gmail.com>
2024-08-12 23:20:34 +0530

    server side changes

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 import express, { response } from 'express'
 import bodyParser from 'body-parser';
 // import routers from './src/routes/routes.js';
-// import axios from 'axios';
+import axios from 'axios';
 const app = express();
 dotenv.config();
 
@@ -19,37 +19,42 @@ app.get("/", (req, res) => {
 
 app.post('/api/indiamart/:key', async (req, res) => {
   try {
-    const secretKey = req.params.key; // Capture the key from the URL
-    console.log(`Received key: ${secretKey}`);
-
-    const { RESPONSE } = req.body;
-
-    // Log the RESPONSE to ensure it contains the expected data
-    console.log('RESPONSE:', RESPONSE);
+    const IndiaMart = req.body;
 
     // Extract fields from RESPONSE
-    const SENDER_NAME = RESPONSE.SENDER_NAME;
-    const SENDER_EMAIL = RESPONSE.SENDER_EMAIL;
+    const Company = IndiaMart.RESPONSE.SENDER_COMPANY;
+    const Last_Name = IndiaMart.RESPONSE.SENDER_NAME;
+    const Mobile = IndiaMart.RESPONSE.SENDER_MOBILE;
+    const Lead_Source = "IndiaMart";
+    const Email = IndiaMart.RESPONSE.SENDER_EMAIL;
+    const Street = IndiaMart.RESPONSE.SENDER_ADDRESS;
+    const City = IndiaMart.RESPONSE.SENDER_CITY;
+    const State = IndiaMart.RESPONSE.SENDER_STATE;
+    const Zip_Code = IndiaMart.RESPONSE.SENDER_PINCODE;
 
-    // Log extracted fields
-    console.log('SENDER_NAME:', SENDER_NAME);
-    console.log('SENDER_EMAIL:', SENDER_EMAIL);
 
-    // // Prepare data for Zoho CRM
-    // const zohoData = {
-    //   SENDER_NAME,
-    //   SENDER_EMAIL,
-    // };
+    // Prepare data for Zoho CRM
+    const zohoData = {
+      Company,
+      Last_Name,
+      Mobile,
+      Lead_Source,
+      Email,
+      Street,
+      City,
+      State,
+      Zip_Code,
+    };
 
-    // // Log zohoData to ensure it's correct
-    // console.log('zohoData:', zohoData);
+    // Log zohoData to ensure it's correct
+    console.log('zohoData:', zohoData);
 
-    // // Send data to Zoho CRM function via API
-    // const response = await axios.post(process.env.ZOHO_API_URL, zohoData, {
-    //   headers: {
-    //     'Content-Type': 'application/json'
-    //   }
-    // });
+    // Send data to Zoho CRM function via API
+    const response = await axios.post(process.env.ZOHO_API_URL, zohoData, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
 
     console.log('Response from Zoho CRM:', response.data);
 
