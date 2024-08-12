1655ee096794633ac2db6f0da9cd44035e9400a9
sg313 <sachin914597@gmail.com>
2024-08-12 22:42:49 +0530

    server side changes

--- server/app.js
+++ server/app.js
@@ -30,43 +30,34 @@ app.post('/api/indiamart/:key', async (req, res) => {
     console.log('STATUS:', STATUS);
     console.log('RESPONSE:', RESPONSE);
 
+
     // Prepare data to be sent to Zoho CRM
-    // const SENDER_NAME = RESPONSE.SENDER_NAME;
-    // const SENDER_EMAIL = RESPONSE.SENDER_EMAIL;
-    
-    // const zohoData = {
-    //   SENDER_NAME,
-    //   SENDER_EMAIL,
-    // };
+    const SENDER_NAME = RESPONSE.SENDER_NAME;
+    const SENDER_EMAIL = RESPONSE.SENDER_EMAIL;
+    // Zoho CRM API endpoint with your API key
+    const zohoData = {
+      SENDER_NAME,
+      SENDER_EMAIL,
 
-    // Try to send data to Zoho CRM function via API
-    // try {
-    //   const response = await axios.post(process.env.ZOHO_API_URL, zohoData, {
-    //     headers: {
-    //       'Content-Type': 'application/json'
-    //     }
-    //   });
+    };
+    // Send data to Zoho CRM function via API
+    const response = await axios.post(process.env.ZOHO_API_URL, zohoData, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
 
-    //   console.log('Response from Zoho CRM:', response.data);
+    console.log('Response from Zoho CRM:', response.data);
 
-    //   // Respond with success if Zoho CRM API call is successful
-    //   res.status(200).json({ code: 200, status: 'Success', message: 'Lead received successfully' });
 
-    // } catch (axiosError) {
-    //   console.error('Error sending data to Zoho CRM:', axiosError);
 
-    //   // Respond with an error message specific to the Zoho CRM API call
-    //   res.status(500).json({ code: 500, status: 'Zoho CRM Error', message: 'Failed to send data to Zoho CRM' });
-    // }
-    
+    res.status(200).json({ code: 200, status: 'Success', message: 'Lead received successfully' });
   } catch (error) {
     console.error('Error processing webhook:', error);
-
-    // Respond with an error message for general errors
+    // Respond with an error message
     res.status(500).json({ code: 500, status: 'Internal Server Error', message: 'An error occurred' });
   }
 });
-
 
 
 
